c8bfe3d970cf9e93a02d3a586db89e464dbcfe11
utkarshkrr <utkarshkumar055@gmail.com>
2025-08-21 08:05:26 +0530

    optimized post details for smaller screens

--- client/src/components/PostDetails/styles.js
+++ client/src/components/PostDetails/styles.js
@@ -7,6 +7,14 @@ export default makeStyles((theme) => ({
     width: '100%',
     maxWidth: '600px',
     maxHeight: '600px',
+    [theme.breakpoints.down('sm')]: {
+      width: '100%',
+      height: 'auto',
+      objectFit: 'cover',
+      maxWidth: '90%',
+      maxHeight: '60%',
+      alignSelf: 'center'
+    },
   },
   details: {
     display: 'flex',
@@ -58,7 +66,7 @@ export default makeStyles((theme) => ({
     borderRadius: '15px',
     overflow: 'hidden',
     boxShadow: '0 4px 20px rgba(0,0,0,0.1)',
-    backgroundColor:'#fffff8ff',
+    backgroundColor: '#fffff8ff',
     cursor: 'pointer',
     width: '250px',
     transition: 'transform 0.2s ease-in-out',
@@ -107,7 +115,7 @@ export default makeStyles((theme) => ({
     },
   },
   commentButton: {
-    color:'#fff',
+    color: '#fff',
     backgroundColor: '#8c6841ff',
     "&:hover": {
       backgroundColor: "#704e2aff",
