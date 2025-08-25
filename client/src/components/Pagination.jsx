2c22fdce84e98e5e72bf3694d66acbf3c7e6976d
utkarshkrr <utkarshkumar055@gmail.com>
2025-08-25 19:13:23 +0530

    fixed pagination

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -6,30 +6,37 @@ import { useDispatch, useSelector } from "react-redux";
 import { getPosts } from "../actions/posts";
 import useStyles from './styles';
 
-
-
-const Paginate = ({ page }) =>{
-
-    const { numberOfPages } = useSelector((state) => state.posts)
-
+const Paginate = ({ page }) => {
+    const { numberOfPages } = useSelector((state) => state.posts);
     const classes = useStyles();
     const dispatch = useDispatch();
 
     useEffect(() => {
-        if(page) dispatch(getPosts(page));
+        if (page) dispatch(getPosts(page));
     }, [page, dispatch]);
 
-    return(
-        <Pagination classes={ {ul: classes.ul }}
+    // Added function to scroll to the top
+    const handlePageChange = () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+
+    return (
+        <Pagination
+            classes={{ ul: classes.ul }}
             count={numberOfPages}
             page={Number(page) || 1}
             variant="outlined"
             color="primary"
             renderItem={(item) => (
-                <PaginationItem {...item} component={Link} to={`/posts?page=${item.page}`} />
+                <PaginationItem 
+                    {...item} 
+                    component={Link} 
+                    to={`/posts?page=${item.page}`}
+                    onClick={handlePageChange} // Added onClick to scroll
+                />
             )}
         />
-    )
-}
+    );
+};
 
 export default Paginate;
