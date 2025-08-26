a33a566e88cdbaee6b85e0dd74d1640b3ff60503
utkarshkrr <utkarshkumar055@gmail.com>
2025-08-26 11:29:37 +0530

    increased post limit

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -8,7 +8,7 @@ export const getPosts = async (req, res) => {
   const { page } = req.query;
 
   try {
-    const LIMIT = 9;
+    const LIMIT = 30;
     const startIndex = (Number(page) - 1) * LIMIT; // get starting index of every page
 
     const total = await PostMessage.countDocuments({});
